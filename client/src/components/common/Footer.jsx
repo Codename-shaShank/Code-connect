87c7a4b1febcb6103aa7687d6a4760ab31497dfc
Shashank Jukuri <shashankjukuri@gmail.com>
2025-10-30 20:27:03 +0530

    A small change

--- client/src/components/common/Footer.jsx
+++ client/src/components/common/Footer.jsx
@@ -4,10 +4,10 @@ function Footer() {
             <span>
                 Build with ❤️ by{" "}
                 <a
-                    href="https://github.com/sahilatahar"
+                    href="https://github.com/Codename-shaShank"
                     className="text-primary underline underline-offset-1"
                 >
-                    sahilatahar
+                    Shashank
                 </a>
             </span>
         </footer>
